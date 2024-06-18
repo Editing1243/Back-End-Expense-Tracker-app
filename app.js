63412d1e0824fd631cfc4af3a77b04a4cabce854
Stefan <stefan.olteanu1995@yahoo.ro>
2024-06-18 17:28:00 +0300

    inteles rute, setat Postman si facut sa primesc raspuns de la server

--- app.js
+++ app.js
@@ -1,20 +1,64 @@
 // Require Modules
+const StatusCodes = require(`http-status-codes`);
 const express = require("express");
 const app = express();
 
 // Middleware
 app.use(express.json());
 
-// Port/Routes
+// accounts placeholder to test and understand http methods
+let accounts = [
+  {
+    id: 1,
+    username: "paulhal",
+    role: "admin",
+  },
+  {
+    id: 2,
+    username: "johndoe",
+    role: "guest",
+  },
+  {
+    id: 3,
+    username: "sarahjane",
+    role: "guest",
+  },
+];
+
+// id array egalizat cu id de params
+
+// Setting up routes/defining http Methods
+app.get(`/`, (req, res) => {
+  res.send(`Am trimis info de la server catre tine. IN SFARSIT !`);
+});
+
+app.get(`/accounts`, (req, res) => {
+  res.json(accounts);
+});
+
+app.get(`/accounts/:id`, (req, res) => {
+  const accountId = Number(req.params.id);
+  const getAccountByID = accounts.find((acc) => acc.id === accountId);
+
+  if (!accountId) {
+    res
+      .status(StatusCodes.NOT_FOUND)
+      .send(`Account not found. Please try again`);
+  } else {
+    res.json(getAccountByID);
+  }
+});
+
+// Creating port and listen to the server
 const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-// middleware cu json pentru request.body la http methods
-// make sure the port/server/postman works and is able to send and receive data
-// create http methods post/get/delete
-// define routes
-// connection to database + CRUD operations
-// vb Tomas sa zica daca e ok si cum mai abordez in continuare
+// define http methods + mutat metodele in modul separat pentru routes
+// connection to database + CRUD operations + data schema + insert data in the first place
+//
+// impart structura pe module
+// ma apuc de implementat features
+// vb Tomas sa zica daca e ok si cum mai abordez in continuare la etapa de features
