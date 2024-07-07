aae3e9a3ce67bea70514527a26e01c2c3cc0031e
Stefan <stefan.olteanu1995@yahoo.ro>
2024-07-07 16:34:27 +0300

    db 1

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // Require Modules
 
 const express = require("express");
+const mongoose = require(`mongoose`);
+
+require(`dotenv`).config();
 const app = express();
 
 const accountRoutes = require("./routes/accountsRoutes");
+const MONGO_URI = process.env.MONGO_URI;
 
 // Middleware
 app.use(express.json());
@@ -12,33 +16,22 @@ app.use(express.urlencoded({ extended: true }));
 // custom routes
 app.use("/", accountRoutes);
 
-// Creating port and listen to the server
-const PORT = 5000;
+// Connect database
+mongoose
+  .connect(MONGO_URI, {})
+  .then(() => console.log(`Database conection success`))
+  .catch((err) => console.log(err));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Creating port and listen to the server
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
-// 2) MongoDB Atlas :
+//////////////////////////////////////////////////////////////////////////////////
 
-// - Cum populez proiectul express app cu date
-
-// - Cum conectez Atlas la proiect
-
-// - Sa imi afișeze datele din proiect în timp real/sa reflecte schimbarile din datele din proiect în Atlas in real time
-
-// - How to Define my data Schema
-
-// 3) Features
-
-// - branch separat pentru feature
-
-// - Login + register
-
-// - Jason Web token
-
-// - testat
-
+// OBIECTIVE :
 // 4) ! Sa nu uit sa creez branch-uri noi pentru features diferite și sa le unesc cu cel de main dupa ce termin cu unul !
 
 // - Vad care e treaba cu Auth
@@ -48,3 +41,22 @@ app.listen(PORT, () => {
 // - când consider ca am făcut tot ce am putut intreb Google/Chat GPT ce mai pot implementa la Expense tracker app
 
 // - când nu mai am și nu mai am idei îl anunț pe Tomas ca nu mai știu ce sa fac și sa se uite pe ce am făcut pana acum
+
+// INTREBARI :
+// --- În a nodejs express app there are CRUD operations.
+// --- We have a schema with the model "Item". What is item.save, item.UpdateOne ? How do you call these
+
+// ---Show me all and every methods that are used to interact with the MongoDB database,
+// particularly for performing CRUD (Create, Read, Update, Delete) operations.
+
+// --- What can I log to see all The methods. If I log The Item obiect will I see all this methods. ?
+// If not, what obiect contains all these methods so I can log it and see them in the VS Code terminal ?
+
+//////////////////////////////////////////////////////
+
+// password - QHzGF23kZtaXLuSv -
+// user - stefanolteanu1995
+// connection strting -
+// mongodb+srv://stefanolteanu1995:0ydGt7tvI2oEdA4r@cluster0.hzxxrdi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+// my updated string -
+// MONGO_URI=mongodb+srv://stefanolteanu1995:0ydGt7tvI2oEdA4r@cluster0.hzxxrdi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
--- controllers/accountsControllers.js
+++ controllers/accountsControllers.js
@@ -1,96 +1,43 @@
 const StatusCodes = require(`http-status-codes`);
+const Account = require(`../models/model`);
 
-let accounts = [
-  {
-    id: 1,
-    username: "paulhal",
-    role: "admin",
-  },
-  {
-    id: 2,
-    username: "johndoe",
-    role: "guest",
-  },
-  {
-    id: 3,
-    username: "sarahjane",
-    role: "guest",
-  },
-];
+const createAccount = async (req, res) => {
+  const { id, name, role } = req.body;
 
-const getHomepage = async (req, res) => {
-  res.json(accounts);
-};
-
-const getAllAccounts = async (req, res) => {
-  res.json(accounts);
-  console.log(accounts);
-};
-
-const getAccountId = async (req, res) => {
-  const accountId = Number(req.params.id);
-  const getAccountId = accounts.filter((acc) => acc.id === accountId);
-
-  if (!accountId) {
-    res.status(StatusCodes.NOT_FOUND).send(`Cannot find account Id`);
-  } else {
-    res.json(getAccountId);
+  if (
+    !id ||
+    typeof id !== `number` ||
+    !name ||
+    typeof name !== `string` ||
+    !role ||
+    typeof role !== `string`
+  ) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .send(`request does not match mongoose data model`);
   }
-};
 
-const getAccountName = async (req, res) => {
-  const accountName = String(req.params.name);
-  const getAccountName = accounts.filter((acc) => acc.username === accountName);
+  try {
+    const existingAccount = await Account.findOne({ id });
+    console.log(existingAccount);
+    if (existingAccount) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .send(`Id was allready taken by another user`);
+    }
 
-  if (!accountName) {
-    res.status(StatusCodes.NOT_FOUND).send(`Cannot find account username`);
-  } else {
-    res.json(getAccountName);
-  }
-};
-
-const getAccountRole = async (req, res) => {
-  const accountRole = String(req.params.role);
-  const getAccountRole = accounts.filter((acc) => acc.role === accountRole);
-
-  if (!accountRole) {
-    console.log(`not found`);
-    res.status(StatusCodes.NOT_FOUND).send(`Cannot find account role `);
-  } else {
-    console.log(`found`);
-    res.json(getAccountRole);
-  }
-};
-
-const post = async (req, res) => {
-  const newAccount = req.body;
-  accounts.push(newAccount);
-  res.json(newAccount);
-  console.log(newAccount);
-};
-
-const put = async (req, res) => {
-  const name = req.params.name;
-  const body = req.body;
-  const account = accounts.find((acc) => acc.username === name);
-
-  if (!account) {
-    res.status(StatusCodes.NOT_FOUND).send(`account not found by usernamme`);
-  } else {
-    const updateAccount = { ...account, ...body };
-    const index = accounts.indexOf(account);
-    accounts[index] = updateAccount;
-
-    res.json(updateAccount);
+    const newAccount = new Account({ id, name, role });
+    // console.log(newAccount);
+    const savedAccount = await newAccount.save();
+    // console.log(savedAccount);
+    res.status(StatusCodes.CREATED).json(savedAccount);
+    console.log(`Account Created`);
+  } catch (err) {
+    console.error(err);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(`Internal server error`);
   }
 };
 
 module.exports = {
-  getAllAccounts,
-  getHomepage,
-  getAccountId,
-  getAccountName,
-  getAccountRole,
-  post,
-  put,
+  createAccount,
 };
